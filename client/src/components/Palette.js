563ea20cda9015ce9413992c955a1c7be758f6de
MertCiril <cirilmert@gmail.com>
2023-02-02 14:07:01 +0300

    1.0.0

---
+++ client/src/components/Palette.js
@@ -0,0 +1,17 @@
+import {useState} from 'react'
+import { send } from '../socketApi'
+
+function Palette() {
+  const [color, setColor] = useState('');
+  return (
+    <div className='Palette'>
+      <input type="color" value={color} onChange={
+        (e)=> setColor(e.target.value)
+      }/>
+      <button onClick={()=>send(color)}>Click</button>
+      {color}
+    </div>
+  )
+}
+
+export default Palette
